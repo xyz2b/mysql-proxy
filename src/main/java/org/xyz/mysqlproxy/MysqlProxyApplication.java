a1b51644b4997b10aa5214ffea7988d1248dd4ec
xyz2b <jiaohongyua@gmail.com>
2022-12-24 20:36:27 +0800

    init

---
+++ src/main/java/org/xyz/mysqlproxy/MysqlProxyApplication.java
@@ -0,0 +1,13 @@
+package org.xyz.mysqlproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysqlProxyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/xyz/mysqlproxy/MysqlProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xyz.mysqlproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlProxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
